748d35f6a2612ef446fcffb674eb4264384a555d
Xiaokuan Ye <144287025+iaouan@users.noreply.github.com>
2023-10-31 16:21:43 -0400

    final upload of the package

--- prime_xy2598/tests/test_prime_xy2598.py
+++ prime_xy2598/tests/test_prime_xy2598.py
@@ -1,5 +1,7 @@
 from prime_xy2598 import prime_xy2598
-def test_is_prime():
+import pytest
+from prime import is_prime
 
-        assert prime_sc4815.is_prime(2) == True
-        assert prime_sc4815.is_prime(9) == False
+def test_is_prime():
+    assert is_prime(2) == True
+    assert is_prime(4) == False
